80ce69d7b07dd1fda99cf4bb84d9b54e70ae7f53
donnie-groot <donniegroot@gmail.com>
2026-04-24 22:42:34 +0000

    feat: inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+###### built in imports ######
+
+###### 3rd party imports ######
+import pygame
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+
+if __name__ == "__main__":
+    main()
+
