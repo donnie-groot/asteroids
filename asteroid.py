ff318c26c87455229778dcf19f7099aaad8129a0
donnie-groot <donniegroot@gmail.com>
2026-04-30 21:58:36 +0000

    feat: added split method so when u shoot a asteroid it splits into smaller ones

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 #imports
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self,):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        a = self.velocity.rotate(angle)
+        b = self.velocity.rotate(-angle)
+
+        new_rad = self.radius - ASTEROID_MIN_RADIUS
+
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_rad)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_rad)
+        new_asteroid_1.velocity = a * 1.2
+        new_asteroid_2.velocity = b * 1.2
+
+
--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
                 for shot in shots:
                     if shot.collides_with(asteroid):
                         log_event("asteroid_shot")
-                        asteroid.kill()
+                        asteroid.split()
                         shot.kill()
 
 
